11954d618f388bc302e808ef2df024ac0d6b70d7
mgalloway20 <mgalloway@umass.edu>
2020-09-06 13:31:03 +0000

    adding old files

---
+++ lab1_MarleeGalloway.R
@@ -0,0 +1,39 @@
+3+3
+
+###Use Alt+- to generate "<-" with automatic spaces
+###Character objects must have "", ex: "ACTG"
+###Vectors must be preceded by "c" and put in parentheses
+
+###Exercise 1
+
+
+x <- 3
+y <- 15
+x+y
+y-x
+x*y
+y/x
+
+
+###Exercise 2
+
+
+x <- 2
+y <- 3
+x^5>y^4
+
+
+###Exercise 3
+
+
+x <- c(22,62,148,43,129)
+sum(x)
+404/x
+
+
+###Exercise 4
+
+
+x <- c("A","T","C","G")
+sort(x)
+
